9e73e084d8b98e14b4b6ebe73e42b93a858250e3
Dmitry Romanov <me@wutiarn.ru>
2016-07-06 19:59:56 +0300

    WIP

--- src/main/kotlin/ru/wutiarn/flibusta/Main.kt
+++ src/main/kotlin/ru/wutiarn/flibusta/Main.kt
@@ -1,8 +1,14 @@
 import ru.wutiarn.flibusta.models.FlibustaStorage
+import java.io.File
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
     val flibustaStorage = FlibustaStorage(Paths.get("data"))
-    val convertedBook = flibustaStorage.getBook(367300, "mobi")
+    val observable = flibustaStorage.getBook(367300, "epub")
+    observable.subscribe {
+        File("q.epub").writeBytes(it)
+    }
+
+    Thread.sleep(1000*1000)
 }
 
--- src/main/kotlin/ru/wutiarn/flibusta/models/FlibustaStorage.kt
+++ src/main/kotlin/ru/wutiarn/flibusta/models/FlibustaStorage.kt
@@ -1,7 +1,8 @@
 package ru.wutiarn.flibusta.models
 
+import rx.Observable
+import rx.schedulers.Schedulers
 import java.io.File
-import java.io.FileNotFoundException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
@@ -16,27 +17,37 @@ class FlibustaStorage(baseDir: Path) {
                 .toList()
     }
 
-    fun getBook(id: Int): ByteArray {
-        return zips.firstOrNull { it.contains(id) }
-                ?.getBookStream(id)
-                ?.readBytes() ?: throw FileNotFoundException()
+    private fun getBook(id: Int): Observable<ByteArray> {
+        return Observable.just(1)
+                .observeOn(Schedulers.io())
+                .map {
+                    println("Start on ${Thread.currentThread().name}")
+                    zips.first { it.contains(id) }
+                            .getBookStream(id)
+                            .readBytes()
+                }
     }
 
-    fun getBook(id: Int, format: String): ByteArray {
-        val bookBytes = getBook(id)
+    fun getBook(id: Int, format: String): Observable<ByteArray> {
+        val bookObservable = getBook(id)
 
-        if (format == "fb2") return bookBytes
+        if (format == "fb2") return bookObservable
 
-        val inTempFile = File.createTempFile(UUID.randomUUID().toString(), ".fb2")
-        val outTempFile = File.createTempFile(UUID.randomUUID().toString(), ".$format")
-        inTempFile.writeBytes(bookBytes)
+        return bookObservable
+                .observeOn(Schedulers.computation())
+                .map {
+            println("Convert on ${Thread.currentThread().name}")
+            val inTempFile = File.createTempFile(UUID.randomUUID().toString(), ".fb2")
+            val outTempFile = File.createTempFile(UUID.randomUUID().toString(), ".$format")
+            inTempFile.writeBytes(it)
 
-        Runtime.getRuntime().exec(arrayOf("ebook-convert", inTempFile.absolutePath, outTempFile.absolutePath)).waitFor()
+            Runtime.getRuntime().exec(arrayOf("ebook-convert", inTempFile.absolutePath, outTempFile.absolutePath)).waitFor()
 
-        inTempFile.delete()
-        val res = outTempFile.readBytes()
-        outTempFile.delete()
-        return res
+            inTempFile.delete()
+            val res = outTempFile.readBytes()
+            outTempFile.delete()
+            res
+        }
     }
 
 }
