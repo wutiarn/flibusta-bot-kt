588170198fd2782ab1fe6484de1908764613d995
Dmitry Romanov <me@wutiarn.ru>
2016-11-21 21:44:07 +0300

    CI: build-base.sh hub push

--- ci-base/build-base.sh
+++ ci-base/build-base.sh
@@ -4,6 +4,7 @@ cd "$(dirname "$0")"
 
 VERSION_TAG="$(sha1sum Dockerfile.base | sha1sum | cut -d' ' -f1)"
 BASE_IMAGE_NAME="ci-base"
+HUB_IMAGE_NAME="wutiarn/ci-base-cache:$VERSION_TAG"
 
 echo "Dockerfile hash is $VERSION_TAG"
 
@@ -18,6 +19,11 @@ else
     rm -rf "$CACHE_DIR"
     mkdir -p "$CACHE_DIR"
     docker build -t "$BASE_IMAGE_NAME" -f Dockerfile.base .
+
+    echo "Pushing as $HUB_IMAGE_NAME..."
+    docker tag "$BASE_IMAGE_NAME" "$HUB_IMAGE_NAME"
+    docker push "$HUB_IMAGE_NAME"
+
     echo "Saving built image ($BASE_IMAGE_NAME) to $IMG_TAR_PATH..."
     docker save "$BASE_IMAGE_NAME" > "$IMG_TAR_PATH"
 fi
