dd0d070ad2b3a2486697f70369a48db18a620b7d
Dmitry Romanov <me@wutiarn.ru>
2016-11-21 20:35:08 +0300

    CI: build-base.sh logging

--- ci-base/build-base.sh
+++ ci-base/build-base.sh
@@ -1,18 +1,25 @@
-#!/bin/bash -ve
+#!/bin/bash -e
 
 cd "$(dirname "$0")"
 
 VERSION_TAG="$(sha1sum Dockerfile.base | sha1sum | cut -d' ' -f1)"
-BASE_IMAGE_NAME="wutiarn/ci-base:$VERSION_TAG"
+BASE_IMAGE_NAME="wutiarn/ci-base"
+
+echo "Dockerfile hash is $VERSION_TAG"
 
 CACHE_DIR="~/docker"
 IMG_TAR_PATH="$CACHE_DIR/$VERSION_TAG.tar"
 
 if [[ -e "$IMG_TAR_PATH" ]];
+    echo "Found cached image. Restoring..."
     then docker load -i "$IMG_TAR_PATH"
 else
+    echo "No cache image found. Building new one..."
     rm -rf "$CACHE_DIR"
     mkdir -p "$CACHE_DIR"
     docker build -t "$BASE_IMAGE_NAME" -f Dockerfile.base .
+    echo "Saving built image..."
     docker save "$BASE_IMAGE_NAME" > "$IMG_TAR_PATH"
 fi
+
+echo "DONE"
