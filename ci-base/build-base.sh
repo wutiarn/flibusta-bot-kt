c734c68003521b4bf116952a01d400bce2368ae9
Dmitry Romanov <me@wutiarn.ru>
2016-11-21 20:06:18 +0300

    CI: build-base.sh CACHE_DIR

--- ci-base/build-base.sh
+++ ci-base/build-base.sh
@@ -3,13 +3,14 @@
 VERSION_TAG="$(sha1sum Dockerfile.base | sha1sum | cut -d' ' -f1)"
 BASE_IMAGE_NAME="wutiarn/ci-base:$VERSION_TAG"
 
-CACHE_PATH="~/docker/$VERSION_TAG.tar"
+CACHE_DIR="~/docker"
+IMG_TAR_PATH="$CACHE_DIR/$VERSION_TAG.tar"
 
-if [[ -e "$CACHE_PATH" ]];
-    then docker load -i "$CACHE_PATH"
+if [[ -e "$IMG_TAR_PATH" ]];
+    then docker load -i "$IMG_TAR_PATH"
 else
-    rm -rf ~/docker
-    mkdir -p ~/docker
+    rm -rf "$CACHE_DIR"
+    mkdir -p "$CACHE_DIR"
     docker build -t "$BASE_IMAGE_NAME" -f Dockerfile.base .
-    docker save "$BASE_IMAGE_NAME" > "$CACHE_PATH"
+    docker save "$BASE_IMAGE_NAME" > "$IMG_TAR_PATH"
 fi
