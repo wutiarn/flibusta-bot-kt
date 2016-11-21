8f07bc548d8925271ef6af84537dbb965e05594f
Dmitry Romanov <me@wutiarn.ru>
2016-11-21 22:58:37 +0300

    update_ci_dockerfile_from.sh linux fix

--- ci-base/update_ci_dockerfile_from.sh
+++ ci-base/update_ci_dockerfile_from.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 
 VERSION_TAG=$(cat VERSION_TAG.txt)
 
-sed -i "" "s/\#CI_BASE_IMAGE_HERE/wutiarn\/ci-base-cache:$VERSION_TAG/" "$BASEDIR/$1"
+sed -i "s/\#CI_BASE_IMAGE_HERE/wutiarn\/ci-base-cache:$VERSION_TAG/" "$BASEDIR/$1"
